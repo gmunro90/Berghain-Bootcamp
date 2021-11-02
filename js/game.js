e937611a1ab518008b60069ed2b5709436382a5b
Greg Munro <gregmunro90@gmail.com>
2021-11-02 09:22:49 +0100

    added more questions

--- js/game.js
+++ js/game.js
@@ -32,59 +32,61 @@ myCards = [
 
     correctAnswer: "Detroit, USA"
   },
-
-
-
-  //below questions need formatting as above
   {
     question: "In which era was Techno music developed in?",
-    answers: {
-      a: "1990s",
-      b: "2010s",
-      c: "1980s",
-      d: "2000s"
-    },
-    correctAnswer: "c"
+    answers: [
+      "1990s", "2010s", "1980s","2000s"
+    ],
+    correctAnswer: "1980s"
   },
   {
     question: "In which location was the now closed, world-famous nightclub Space?",
-    answers: {
-      a: "Miami",
-      b: "Ibiza",
-      c: "Mallorca",
-      d: "Manchester"
-    },
-    correctAnswer: "b"
+    answers: [
+      "Miami", "Ibiza", "Mallorca", "Manchester"
+    ],
+    correctAnswer: "Ibiza"
   },
   {
     question: "Who composed the famous techno track 'The Bells'?",
-    answers: {
-      a: "Jeff Mills",
-      b: "Carl Cox",
-      c: "Amelie Lens",
-      d: "Laurent Garnier"
-    },
-    correctAnswer: "a"
+    answers: [
+      "Jeff Mills", "Carl Cox", "Amelie Lens", "Laurent Garnier"
+    ],
+    correctAnswer: "Jeff Mills"
   },
   {
     question: "In which country is Sunwaves Festival held twice a year?",
-    answers: {
-      a: "Italy",
-      b: "China",
-      c: "Romania",
-      d: "Fiji"
-    },
-    correctAnswer: "c"
+    answers: [
+      "Italy", "China", "Romania","Fiji"
+    ],
+    correctAnswer: "Romania"
   },
   {
     question: "Barcelona is home to which of these famous techno DJs?",
-    answers: {
-      a: "Dubfire, Maceo Plex, Marco Carola",
-      b: "Peggy Gou, Green Velvet, Sven Väth",
-      c: "Deborah de Luca, Adam Beyer, Chris Liebing",
-      d: "Ben Klock, Marco Carola, Joseph Capriati"
-    },
-    correctAnswer: "a"
+    answers: [
+      "Dubfire, Maceo Plex, Marco Carola","Peggy Gou, Green Velvet, Sven Väth", "Deborah de Luca, Adam Beyer, Chris Liebing","Ben Klock, Marco Carola, Joseph Capriati"
+    ],
+    correctAnswer: "Dubfire, Maceo Plex, Marco Carola"
+  },
+  {
+    question: "QUESTION 7?",
+    answers: [
+      "ANSWERS 7"
+    ],
+    correctAnswer: "CORRECT ANSWER 7"
+  },
+  {
+    question: "QUESTION 8?",
+    answers: [
+      "ANSWERS 8"
+    ],
+    correctAnswer: "CORRECT ANSWER 8"
+  },
+  {
+    question: "QUESTION 9?",
+    answers: [
+      "ANSWERS 9"
+    ],
+    correctAnswer: "CORRECT ANSWER 9"
   }
 ]
 }
