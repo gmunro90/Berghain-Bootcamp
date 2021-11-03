4ede96d4bf83a4dc9b6f6f9a1c51046cfca27c52
Greg Munro <gregmunro90@gmail.com>
2021-11-03 15:20:47 +0100

    winning and losing logic complete

--- js/game.js
+++ js/game.js
@@ -5,115 +5,110 @@ class Game {
     this.score = 0; //if score.length === 6, WINNER - enter the club
     this.incorrectAnswer = 0; //if incorrectanswer.length === 1, this.lives -1
     this.totalIncorrect = 0; //if totalincorrect.length === 2, GAME OVER
-    this.lives = 2; 
+    this.lives = 2;
     this.isWinner = false;
-    this.questions = [...this.myCards]
-    this.answeredQst = []
-    
+    this.questions = [...this.myCards];
+    this.answeredQst = [];
+
     /*this.progressBar = 0;
     this.timer = 0;*/
   }
 
+  getRandomCard() {
+    //eventually will return RANDOM q, not first index
+    let currentQIndx = Math.floor(Math.random() * this.questions.length);
+    let currentQst = this.questions[currentQIndx];
+    this.answeredQst.push(currentQst);
+    this.questions.splice(currentQIndx, 1);
 
-getRandomCard(){ //eventually will return RANDOM q, not first index
-  let currentQIndx = Math.floor(Math.random()*this.questions.length)
-  let currentQst = this.questions[currentQIndx];
-  this.answeredQst.push(currentQst)
-  this.questions.splice(currentQIndx, 1)
+    console.log("question number", this.questions);
+    console.log("answered qst", this.answeredQst);
 
-  console.log('questtion', this.questions)
-  console.log('answered qst', this.answeredQst)
-  
-  return currentQst;
-}
+    return currentQst;
+  }
 
-scoreUp(){
-  this.score +=1
-}
-//let currentQst = myCards[0];
-//let correctAnswer = currentQst.correctAnswer[getRandomCard(currentQst.correctAnswer)];
+  scoreUp() {
+    this.score += 1;
+  }
 
+  /*resetScore(){ 
+  this.score = 0
+}*/
 
-/*scoreCounter (){
-  //if 
+  takeLife() {
+    this.lives -= 1;
+  }
+
+  /*
 }
 checkIfFinished() {
   if(this.score === 6) this.isWinner = true;
   return this.isWinner
 }*/
 
+  myCards = [
+    {
+      question: "Where was Techno music first pioneered?",
+      answers: ["Ibiza, Spain", "New York, USA", "London, UK", "Detroit, USA"],
+      correctAnswer: "Detroit, USA", // also can gove the index of the correct answer
+    },
 
- myCards = [
-  {
-    question: "Where was Techno music first pioneered?",
-    answers: [
-     "Ibiza, Spain", "New York, USA", "London, UK", "Detroit, USA"
-    ],
-    correctAnswer: "Detroit, USA" // also can gove the index of the correct answer
-  },
-
-  {
-    question: "In which era was Techno music developed in?",
-    answers: [
-      "1990s", "2010s", "1980s","2000s"
-    ],
-    correctAnswer: "1980s"
-  },
-  {
-    question: "In which location was the now closed, world-famous nightclub Space?",
-    answers: [
-      "Miami", "Ibiza", "Mallorca", "Manchester"
-    ],
-    correctAnswer: "Ibiza"
-  },
-  {
-    question: "Who composed the famous techno track 'The Bells'?",
-    answers: [
-      "Jeff Mills", "Carl Cox", "Amelie Lens", "Laurent Garnier"
-    ],
-    correctAnswer: "Jeff Mills"
-  },
-  {
-    question: "In which country is Sunwaves Festival held twice a year?",
-    answers: [
-      "Italy", "China", "Romania","Fiji"
-    ],
-    correctAnswer: "Romania"
-  },
-  {
-    question: "Barcelona is home to which of these famous techno DJs?",
-    answers: [
-      "Dubfire, Maceo Plex, Marco Carola","Peggy Gou, Green Velvet, Sven Väth", "Deborah de Luca, Adam Beyer, Chris Liebing","Ben Klock, Marco Carola, Joseph Capriati"
-    ],
-    correctAnswer: "Dubfire, Maceo Plex, Marco Carola"
-  },
-  {
-    question: "Who is often considered 'The Father of Techno'?",
-    answers: [
-      "Marco Santonastasi", "Enrique Iglesias", "Juan Atkins", "Derrick May"
-    ],
-    correctAnswer: "Juan Atkins"
-  },
-  {
-    question: "What is the most utilized instrument by original techno producers?",
-    answers: [
-      "TR-808", "SH-101", "TB-303", "NI-909"
-    ],
-    correctAnswer: "TR-808"
-  },
-  {
-    question: "In which country is Timewarp Festival held every April?",
-    answers: [
-      "Germany", "France", "Scotland", "Portugal"
-    ],
-    correctAnswer: "Germany"
-  },
-  {
-    question: "Which of these DJs is a regular resident at Berghain?",
-    answers: [
-      "David Guetta", "Ben Klock", "Locomia", "Oxia"
-    ],
-    correctAnswer: "Ben Klock"
-  }
-]
+    {
+      question: "In which era was Techno music developed in?",
+      answers: ["1990s", "2010s", "1980s", "2000s"],
+      correctAnswer: "1980s",
+    },
+    {
+      question:
+        "In which location was the now closed, world-famous nightclub Space?",
+      answers: ["Miami", "Ibiza", "Mallorca", "Manchester"],
+      correctAnswer: "Ibiza",
+    },
+    {
+      question: "Who composed the famous techno track 'The Bells'?",
+      answers: ["Jeff Mills", "Carl Cox", "Amelie Lens", "Laurent Garnier"],
+      correctAnswer: "Jeff Mills",
+    },
+    {
+      question: "In which country is Sunwaves Festival held twice a year?",
+      answers: ["Italy", "China", "Romania", "Fiji"],
+      correctAnswer: "Romania",
+    },
+    {
+      question: "Barcelona is home to which of these famous techno DJs?",
+      answers: [
+        "Dubfire, Maceo Plex, Marco Carola",
+        "Peggy Gou, Green Velvet, Sven Väth",
+        "Deborah de Luca, Adam Beyer, Chris Liebing",
+        "Ben Klock, Marco Carola, Joseph Capriati",
+      ],
+      correctAnswer: "Dubfire, Maceo Plex, Marco Carola",
+    },
+    {
+      question: "Who is often considered 'The Father of Techno'?",
+      answers: [
+        "Marco Santonastasi",
+        "Enrique Iglesias",
+        "Juan Atkins",
+        "Derrick May",
+      ],
+      correctAnswer: "Juan Atkins",
+    },
+    {
+      question:
+        "What is the most utilized instrument by original techno producers?",
+      answers: ["TR-808", "SH-101", "TB-303", "NI-909"],
+      correctAnswer: "TR-808",
+    },
+    {
+      question: "In which country is Timewarp Festival held every April?",
+      answers: ["Germany", "France", "Scotland", "Portugal"],
+      correctAnswer: "Germany",
+    },
+    {
+      question: "Which of these DJs is a regular resident at Berghain?",
+      answers: ["David Guetta", "Ben Klock", "Locomia", "Oxia"],
+      correctAnswer: "Ben Klock",
+    },
+  ];
 }
--- js/main.js
+++ js/main.js
@@ -1,30 +1,36 @@
-function buildCardHTML(card){
-  let prompt = card.question
+function buildCardHTML(card) {
+  let prompt = card.question;
 
   const htmlAnswer = card.answers.reduce(function (html, answer) {
-    html += '<li>' + answer + '</li>';
+    html += "<li>" + answer + "</li>";
     return html;
-  }, '');
+  }, "");
 
-  return `${prompt}` + htmlAnswer;//why not have this in backtick string too?
-
+  return `${prompt}` + htmlAnswer; //why not have this in backtick string too?
 }
-
 
 // General function that will update the HTML content dynamically
 const buildDomGame = (cardHTML) => {
-    let main = document.querySelector("main");
-    main.innerHTML = `<p>Score: ${game.score} </p>` +"<ol>" + cardHTML + "</ol>";
+  //here you can affect the visuals and html of the game screen
+  let main = document.querySelector("main");
+  main.innerHTML =
+    `
+    <div class="score">
+    <p>Score: ${game.score} </p></div>` +
+    `<p>lives: ${game.lives}</p>` +
+    "<ol>" +
+    cardHTML +
+    "</ol>";
 };
 
 const buildDom = (HTML) => {
   let main = document.querySelector("main");
   main.innerHTML = HTML;
 };
-  
-  // First Screen => Splash Screen
-  const buildSplashScreen = () => {
-    buildDom(`
+
+// First Screen => Splash Screen
+const buildSplashScreen = () => {
+  buildDom(`
     <div class="splash-screen">
       <h1>Berghain Bootcamp</h1>
       <h2 class="prove">Prove your techno music knowledge and earn the right to enter Berghain!</h2>
@@ -44,69 +50,79 @@ const buildDom = (HTML) => {
       </div>
     </div>
     `);
-    const startButton = document.getElementById("start-button");
-    startButton.addEventListener("click", buildGameScreen);
-  };
-    
-   // Second Screen => Game Screen
 
-  const buildGameScreen = () => {
-    const card = game.getRandomCard();
-    const cardHTML = buildCardHTML(card);
-    
-    buildDomGame(cardHTML)
-     
+  const startButton = document.getElementById("start-button");
+  startButton.addEventListener("click", buildGameScreen);
+};
 
-    let choices = document.querySelector("ol")
-    choices.addEventListener('click',function(event) {
-  
-    //let correct = false //IS THERE A WAY I CAN TURN THE BELOW INTO A FUNCTION THAT CAN THEN RETURN IF CORRECT ADD POINT IF INCORRECT TAKE LIFE?
-    if(event.target.innerText === card.correctAnswer) {
-    main = document.querySelector('main')
-    main.innerHTML = (`CORRECT` + `<br><br><button id="next-button">`+ `NEXT QUESTION` + `</button>`)
-    
-    const nextBtn = document.getElementById('next-button')
-    nextBtn.addEventListener('click', buildGameScreen)
-    game.scoreUp()
-    //if abive === blah
+// Second Screen => Game Screen
 
+const buildGameScreen = () => {
+  //logic of the game screen + affect html dependant on actions
+  //const newScore = resetScoreUp();
+  const card = game.getRandomCard();
+  const cardHTML = buildCardHTML(card);
 
-    /*const addPoint = document.getElementById('points-counter')
-    forEach (card.correctAnswer){
-      addPoint.innerText(+=1)
-    }*/
+  buildDomGame(cardHTML);
 
-   } else {
-    main = document.querySelector('main')
-    main.innerHTML = (`INCORRECT you lost a life!<br><br><button id="next-button">NEXT QUESTION</button>`)
+  let choices = document.querySelector("ol");
+  choices.addEventListener("click", function (event) {
+    //let correct = false
+    if (event.target.innerText === card.correctAnswer) {
+      main = document.querySelector("main");
+      main.innerHTML =
+        `CORRECT` +
+        `<br><br><button id="next-button">` +
+        `NEXT QUESTION` +
+        `</button>`;
 
-    const nextBtn = document.getElementById('next-button')
-    nextBtn.addEventListener('click', buildGameScreen)
+      const nextBtn = document.getElementById("next-button");
+      nextBtn.addEventListener("click", buildGameScreen);
+      game.scoreUp(); //adding points
+      //if above === reaches 6, build WINNER html
+      if (game.score === 1) {
+        //console.log('youWIN')
+        main = document.querySelector("main");
+        main.innerHTML =
+          `WINNER` + `<br><button id="otravez">PLAY AGAIN?</button>`;
+        const playAgainBtn = document.getElementById("otravez");
+        playAgainBtn.addEventListener("click", buildGameScreen);
       }
-    })
-  }
+    } else {
+      main = document.querySelector("main");
+      main.innerHTML = `INCORRECT...you lost a life!<br><br><button id="next-button">NEXT QUESTION</button>`;
 
+      const nextBtn = document.getElementById("next-button");
+      nextBtn.addEventListener("click", buildGameScreen);
+      game.takeLife();
+      if (game.lives === 0) {
+        buildGameOver();
+      }
+    }
+  });
+};
 
-
-  // Third Screen => Game Over
-  function buildGameOver() {
+// Third Screen => Game Over
+function buildGameOver() {
   buildDom(`
+
     <section class="game-over">
+
     <h1>Game Over</h1>
     <h3>Sorry, you'll have to settle for just a Döner tonight...</h3>
-    <button id = "game">TRY AGAIN</button>
-    <div class= "pointer"> </div>
+    
+    <br><button id="otravez">PLAY AGAIN?</button>
+    
     </section>
+
     `);
 
-  const restartButton = document.querySelector("button");
-  restartButton.addEventListener("click", buildGameScreen);
+  const playAgainBtn = document.getElementById("otravez");
+  playAgainBtn.addEventListener("click", buildGameScreen);
 }
 
 const game = new Game();
 
-
-
-  // When the window loads, then we will run the "buildSplashScreen" function
-  // "load" waits for the html and JS
-  window.addEventListener("load", buildSplashScreen);
+// When the window loads, then we will run the "buildSplashScreen" function
+// "load" waits for the html and JS
+window.addEventListener("load", buildSplashScreen);
